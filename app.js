57cba13927f5ea9f5be4d129d65a3a434b5ead51
duck_ach <djagmlfk@naver.com>
2022-04-06 16:41:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const player = {
-    name: "heera",
-    points : 10,
-    fat : true,
-
+const calculator = {
+    add :function(a, b){
+        console.log(a + b);
+    },
+    minus :function(a, b){
+        console.log(a - b);
+    },
+    multiply :function(a, b){
+        console.log(a * b)
+    },
+    division :function(a, b){
+        console.log(a / b);
+    },
+    powerOf :function(a, b){
+        console.log(a ** b);
+    },
 };
 
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-
-console.log(player);
-player.points = player.points + 15
-console.log(player.points)
+calculator.add(5, 2);
+calculator.minus(90, 3);
+calculator.multiply(30, 2);
+calculator.division(42, 7);
+calculator.powerOf(2, 8);
