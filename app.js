ea232550a7659e30da905e28382379bb6b12f7c7
duck_ach <djagmlfk@naver.com>
2022-04-11 20:05:13 +0900

    event 3

--- app.js
+++ app.js
@@ -2,4 +2,30 @@ const title = document.querySelector(".hello:first-child h1");
 function handleTitleClick(){
     title.style.color = "pink";
 }
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter() {
+    title.innerText = "Mouth is here!";
+}
+function handleMouseLeave(){
+    title.innerText = "Mouth is gone!";
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy(){
+    alert("copier!");
+}
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+function handleWindowOnline(){
+
+}
+//title.addEventListener("click", handleTitleClick);
+title.onclick = handleTitleClick;
+title.onmouseenter = handleMouseEnter;
+title.onmouseleave = handleMouseLeave;
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
