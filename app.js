7578ae6679e71c34c9b26c088e31a0ec33a73dee
duck_ach <djagmlfk@naver.com>
2022-04-17 23:41:46 +0900

    Events part Two

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput =loginForm.querySelector("#login-form input");
 
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
     event.preventDefault();
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+        event.preventDefault();
+    console.dir(event)
+    alert("clicked!")
+    
+}
+
 
 loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click", handleLinkClick)
+
+handleLinkClick({})
