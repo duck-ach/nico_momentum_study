65edd5b54746e78eefb6b2332e1ad0a872244c32
duck-ach <101095613+duck-ach@users.noreply.github.com>
2022-04-28 15:51:23 +0900

    Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,20 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document. querySelector("#todo-form input");
-//const toDoInput = document.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li")
+    const span = document.createElement("span")
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event) {
  event.preventDefault();
- //toDoInput.classList.add("hidden")
  const newTodo = toDoInput.value;
- console.log(toDoInput.value);
  toDoInput.value = "";
- console.log(newTodo, toDoInput.value);
+ paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
