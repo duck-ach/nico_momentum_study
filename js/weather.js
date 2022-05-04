efff03b999f99979794fddecbf101b0476ce5254
duck-ach <101095613+duck-ach@users.noreply.github.com>
2022-05-04 23:32:19 +0900

    Weather API 2 complete

--- js/weather.js
+++ js/weather.js
@@ -3,22 +3,19 @@ const city = document.querySelector("#weather span:last-child");
 const API_KEY = "e9c4601c1e07994ec6b1d0230275db58";
 
 function onGeoOk(position) {
-    const lat = position.coords.latitude;
+  const lat = position.coords.latitude;
   const lng = position.coords.longitude;
   console.log("You live in", lat, lng);
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
       city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
-function onGeoError(){
-    alert("Can't find you. No weather for you.")
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
 }
-
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-
-//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}
